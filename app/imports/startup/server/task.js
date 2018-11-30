e3bc6e44f038f68461b472626071e2f85ed9166d
Jon Lau <laujh@hawaii.edu>
2018-11-30 11:50:24 -1000

    fixed task & message not loading

--- app/imports/startup/server/task.js
+++ app/imports/startup/server/task.js
@@ -4,7 +4,7 @@ import { Tasks } from '../../api/task/task';
 /** This subscription publishes only the documents associated with the logged in user */
 Meteor.publish('Tasks', function publish() {
   if (this.userId) {
-    return Tasks.find({ members:  Meteor.user().username });
+    return Tasks.find();
   }
   return this.ready();
 });
