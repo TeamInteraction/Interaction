a17072515bbc2d2bb3097cc58d61d0a60591e62e
Jon Lau <laujh@hawaii.edu>
2018-12-05 08:51:25 -1000

    landing text fix

--- app/imports/ui/pages/ContactUs.jsx
+++ app/imports/ui/pages/ContactUs.jsx
@@ -11,36 +11,19 @@ export default class ContactUs extends React.Component {
               <Grid centered>
                 <Grid.Row>
                   <Grid.Column width={3}>
-                    Kyle Chan
+                    Eugene Lao
                     <hr/>
-                    <List>
-                      <a href="https://kyle-chan.github.io/">
-                        https://kyle-chan.github.io/</a>
-                    </List>
+                  
                   </Grid.Column>
                   <Grid.Column width={3}>
                     Jonathan Lau
                     <hr/>
-                    <List>
-                      <a href="https://jon-lau.github.io/">
-                        https://jon-lau.github.io/</a>
-                    </List>
-                  </Grid.Column>
-                  <Grid.Column width={3}>
-                    Arnold Shek
-                    <hr/>
-                    <List>
-                      <a href="https://arnoldshek.github.io/">
-                        https://arnoldshek.github.io/</a>
-                    </List>
+                   
                   </Grid.Column>
                   <Grid.Column width={3}>
                     Akira Vernon
                     <hr/>
-                    <List>
-                      <a href="https://akirav.github.io/">
-                        https://akirav.github.io/</a>
-                    </List>
+                    
                   </Grid.Column>
                 </Grid.Row>
               </Grid>
